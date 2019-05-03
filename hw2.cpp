4faddc1e9507d6dd616ffe423786ddaeda33552c
shaharshalev <shaharshalev@campus.technion.ac.il>
2019-05-03 12:00:27 +0300

    impl first

--- hw2.cpp
+++ hw2.cpp
@@ -1,10 +1,12 @@
 #include <stdlib.h>
 #include "hw2.h"
 
+using namespace std;
+
 bool isToken(int token){
     return token>=IMPORTANT;
 }
-bool isRhsNullable(grammar_rule rule,std::vector<bool> &nullableVector){
+bool isRhsNullable(grammar_rule rule,vector<bool> &nullableVector){
     for(int i=0;i<rule.rhs.size();i++){
         int var=rule.rhs[i];
         if(isToken(var))
@@ -15,8 +17,8 @@ bool isRhsNullable(grammar_rule rule,std::vector<bool> &nullableVector){
     return true;
 }
 
-void compute_nullable(){
-    std::vector<bool> nullables(NONTERMINAL_ENUM_SIZE,false);
+vector<bool> getNullable(){
+    vector<bool> nullables(NONTERMINAL_ENUM_SIZE,false);
     for(int i=0;i<=grammar.size();i++){
         for(int j=0;j<grammar.size();j++){
             grammar_rule rule=grammar[j];
@@ -26,7 +28,66 @@ void compute_nullable(){
 
         }
     }
-    print_nullable(nullables);
+
+    return nullables;
+}
+
+void compute_nullable(){
+    print_nullable(getNullable());
+}
+
+set<tokens> unionSet(const set<tokens> &s1,const set<tokens> &s2){
+    set<tokens> uSet;
+    set<tokens>::iterator it;
+    for (it = s1.begin(); it != s1.end(); ++it) {
+          uSet.insert(*it);
+     }
+    for (it = s2.begin(); it != s2.end(); ++it) {
+          uSet.insert(*it);
+     }
+
+     return uSet;
+}
+
+bool isNullable( vector<bool> &nullables,int index){
+    if(index >= nullables.size()) return false;
+    return nullables[index];
+}
+
+
+vector< set<tokens> > first(){
+    vector< set<tokens> > firstCollection(NONTERMINAL_ENUM_SIZE);
+    vector<bool> nullables = getNullable();
+    bool isChanged;
+    do{
+        isChanged = false;
+        for(int i=0 ; i < grammar.size() ; i++){
+            int left = grammar[i].lhs;
+            if(grammar[i].rhs.size() == 0) continue;
+            int right = grammar[i].rhs[0];
+            int lSize = firstCollection[left].size();
+            if(isToken(right)){
+                firstCollection[left].insert((tokens)right);
+            }else{
+                firstCollection[left] = unionSet(firstCollection[left],firstCollection[right]);
+                
+                for(int j =1 ;j < grammar[i].rhs.size() && isNullable(nullables, right) ; j++){
+                    right = grammar[i].rhs[j];
+                    if(isToken(right)){
+                        firstCollection[left].insert((tokens)right);
+
+                    }else{
+                        firstCollection[left] = unionSet(firstCollection[left],firstCollection[right]);
+                    }
+                    
+                }
+            }
+            if(lSize != firstCollection[left].size())
+                isChanged = true;
+        }
+    }while(isChanged);
+
+    return firstCollection;
 }
 
 
@@ -35,7 +96,7 @@ void compute_nullable(){
  * calls print_first when finished
  */
 void compute_first(){
-    exit(1);
+    print_first(first());
 }
 
 /**
